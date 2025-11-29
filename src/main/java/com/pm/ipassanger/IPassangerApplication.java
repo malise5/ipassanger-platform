efb1dfd9b1290685942a1df48004b43bede5c2ce
Halkano Malise <kudezx@gmail.com>
2025-11-30 00:33:31 +0300

    spinned iPassanger project

---
+++ src/main/java/com/pm/ipassanger/IPassangerApplication.java
@@ -0,0 +1,13 @@
+package com.pm.ipassanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPassangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPassangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/ipassanger/IPassangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.ipassanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPassangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
